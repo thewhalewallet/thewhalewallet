8f69a4c9a4258f13acec856ce5e46576c3501a5d
Jonathan Conn <jonathansconn@gmail.com>
2023-02-28 20:11:30 -0700

    init framework

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
