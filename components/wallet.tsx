707aa651fe3767b42ca36575544b3499dac52cf9
Jonathan Conn <jonathansconn@gmail.com>
2023-02-28 20:39:21 -0700

    added rainbowkit skeleton and configured alch

---
+++ components/wallet.tsx
@@ -0,0 +1,9 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+export default function WalletConnect() {
+  return (
+    <div>
+      <ConnectButton />;
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,26 @@ import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_KEY as string }), publicProvider()]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'WhaleWallet',
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />;
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
