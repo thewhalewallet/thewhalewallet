b8ce0c37592635eba1a3a665658319141f8f4292
Jonathan Conn <jonathansconn@gmail.com>
2023-03-03 13:34:10 -0700

    removed component from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import '@/styles/login.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import RouteGuard from '@/components/RouteGuard';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
