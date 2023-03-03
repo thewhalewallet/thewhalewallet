7d44ae3f7b6f72b084eb413351e52f717d1b72ee
ThomasDumouchel <thomas.dumouchel@gmail.com>
2023-03-02 21:54:51 -0700

    Fixed bug with Dynamic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,16 +11,20 @@ import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 //
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DynamicContextProvider
-    settings={{
-      appLogoUrl:
-        'public/theWaleWalletLogo.jpeg',
-      appName: 'The Whale Wallet',
-      environmentId: '18fa6c3f-9025-4c9b-8f5b-02eff904aa72'
-   }}>
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
 
-    <DynamicWidget /> 
-  </DynamicContextProvider>
+      <DynamicContextProvider
+        settings={{
+          appLogoUrl:
+            'public/theWaleWalletLogo.jpeg',
+          appName: 'The Whale Wallet',
+          environmentId: '18fa6c3f-9025-4c9b-8f5b-02eff904aa72'
+        }}
+      >
+
+      <DynamicWidget /> 
+      </DynamicContextProvider>
+    </>
   );
 }
