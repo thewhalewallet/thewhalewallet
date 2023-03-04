e50bc8b75eeb2bca90ebf39d83071cb0ad41f36e
Jonathan Conn <jonathansconn@gmail.com>
2023-03-04 16:12:42 -0700

    added tx data from plaid to db

--- components/types/IUser.ts
+++ components/types/IUser.ts
@@ -1,14 +1,12 @@
 import IWallet from "./IWallet";
 import IContact  from "./IContact";
 import IPlaidAccount from "./IPlaidAccount";
+import IPlaidTx from "./IPlaidTx";
 export default interface IUser {
     _id: string;
     email: string;
     name: string;
     image: string;
-    plaid_access_token: string;
-    plaid_item_id: string;
-    plaid_account: IPlaidAccount[];
     wallets: IWallet[];
     contacts: IContact[];
     plaid_access_token: string;
