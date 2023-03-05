79b254ee564d36a4736fe88f1f661adb9708d37e
Jonathan Conn <jonathansconn@gmail.com>
2023-03-05 00:49:13 -0700

    added callback to update wallets from dyn to our db

--- components/utils/wallet.service.ts
+++ components/utils/wallet.service.ts
@@ -5,12 +5,26 @@ import {
     getFollowing
 } from './LensProfile'
 
+const { ethers } = require("ethers");
+
+async function getEnsByWalletAddress(walletAddress: string) {
+    const provider = new ethers.providers.AlchemyProvider('mainnet', process.env.NEXT_PUBLIC_ALCHEMY_KEY);
+    let res = await provider.lookupAddress(walletAddress);
+    if (res) {
+        return res
+    }
+    return '';
+}
+
 async function getLensProfileByWalletAddress(walletAddress: string) {
-    let result = await client.query({
+    let res = await client.query({
         query: defaultProfileByWalletAddress,
         variables: { ethereumAddress: walletAddress }
     });
-    return result.data.defaultProfile;
+    if (res.data.defaultProfile) {
+        return res.data.defaultProfile;
+    }
+    return '';
 }
 
 async function getFollowingByWalletAddress(walletAddress: string) {
@@ -25,4 +39,4 @@ async function getFollowingByWalletAddress(walletAddress: string) {
 
 
 
-export { getLensProfileByWalletAddress, getFollowingByWalletAddress };
+export { getLensProfileByWalletAddress, getFollowingByWalletAddress, getEnsByWalletAddress};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/Navbar';
 import PlaidDisplay from '@/components/plaid/PlaidDisplay';
 import IUser from '@/components/types/IUser';
 import WalletDisplay from '@/components/WalletDisplay';
-import { DynamicContextProvider, useDynamicContext } from '@dynamic-labs/sdk-react';
+import { DynamicConnectButton, DynamicContextProvider, DynamicWidget, useDynamicContext } from '@dynamic-labs/sdk-react';
 import { useState } from 'react';
 import UserHandler from '@/components/utils/UserHandler.service';
 import { PlaidChart } from '@/components/plaid/PlaidChart';
@@ -14,11 +14,13 @@ export default function Dashboard() {
     return (
         <>
             <UserHandler setCurrentUser={setCurrentUser} />
-
-            <Navbar />
+        
+            {/* <Navbar /> */}
             {/* <div className="hero min-h-screen min-w-max bg-base-100"> */}
             <div className='flex grow-1 justify-center bg-slate-300'>
-                <PlaidChart user={currentUser} />
+                {/* <PlaidChart user={currentUser} /> */}
+                <DynamicWidget variant='dropdown'/>
+                <WalletDisplay />
             </div>
 
                 {/* <WalletDisplay /> */}
